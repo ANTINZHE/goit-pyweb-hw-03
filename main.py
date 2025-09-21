20c1c2e0d3db01363e65888b7124a8d3d07d07fd
ANTIN <antonzhelihovskiy@gmail.com>
2025-09-21 12:40:29 +0300

    Add ThreadPoolExecutor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import shutil
 from pathlib import Path
+import concurrent.futures
 
 
 def check_folders():
@@ -28,27 +29,35 @@ def check_folders():
     return trash_folder, finish_folder
 
 
-def sorted_files(trash_folder: Path, finish_folder: Path):
+def sorted_files(trash_folder: Path, finish_folder: Path, executor):
     try:
         for item in sorted(trash_folder.iterdir()):
             if item.is_dir():
-                sorted_files(item, finish_folder)
+                sorted_files(item, finish_folder, executor)
             else:
-                suffix = item.suffix.lstrip(".")
-                ext_folder = finish_folder / suffix
-                ext_folder.mkdir(
-                    parents=True, exist_ok=True
-                )  # Створюється директорія, якщо її немає
-                shutil.copy2(
-                    item, ext_folder / item.name
-                )  # Копіюємо файл за допомогою copy2 з усіма даними
+                executor.submit(copy_files, item, finish_folder)
 
     except PermissionError:
-        print(f"{"Доступ закритий"}")
+        print("Доступ закритий")
+
+
+def copy_files(item, finish_folder: Path):
+    print(f"копіювання файлу {item}")
+    suffix = item.suffix.lstrip(".")
+    ext_folder = finish_folder / suffix
+    ext_folder.mkdir(
+        parents=True, exist_ok=True
+    )  # Створюється директорія, якщо її немає
+    shutil.copy2(
+        item, ext_folder / item.name
+    )  # Копіюємо файл за допомогою copy2 з усіма даними
+    print(f"Завершення копіювання {item}")
 
 
 def main():
-    sorted_files(*check_folders())
+    trash_folder, finish_folder = check_folders()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        sorted_files(trash_folder, finish_folder, executor)
 
 
 if __name__ == "__main__":
